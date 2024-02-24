bc013731b2d9d5895f3c7e71b5f91bbcdb824aa9
Theoreticallyhugo <me@theoreticallyhugo.com>
2024-02-24 22:06:48 +0100

    new script that tests the local dataset instead of remote, with visual aid

---
+++ old_models/new_sota/test_dataset.py
@@ -0,0 +1,52 @@
+import datasets
+from colorama import Fore, Back, Style
+
+ds = []
+print(">>> full_labels")
+ds.append(
+    datasets.load_dataset("essays_SuG", "full_labels", trust_remote_code=True)
+)
+print(">>> spans")
+ds.append(datasets.load_dataset("essays_SuG", "spans", trust_remote_code=True))
+print(">>> simple")
+ds.append(
+    datasets.load_dataset("essays_SuG", "simple", trust_remote_code=True)
+)
+print(">>> sep_tok")
+ds.append(
+    datasets.load_dataset("essays_SuG", "sep_tok", trust_remote_code=True)
+)
+print(">>> sep_tok_full_labels")
+ds.append(
+    datasets.load_dataset(
+        "essays_SuG",
+        "sep_tok_full_labels",
+        trust_remote_code=True,
+    )
+)
+print("done")
+for s in ds:
+    print("")
+    print(s["train"].features)
+    print(s["train"][0])
+
+
+# highlight spans in green
+for i in range(len(ds[1]["test"]["text"])):
+    print(f">>> text {i}")
+    text = ds[1]["test"]["text"][i]
+    cls_tok = ds[1]["test"]["span_begins"][i]
+    sep_tok = ds[1]["test"]["span_ends"][i]
+    for letter in range(len(text)):
+        if letter in cls_tok:
+            # if a span starts here, set fore to green
+            print(Fore.GREEN, end="")
+        print(text[letter], end="")
+        # input(text[letter])
+        if letter in sep_tok:
+            # if a span ends here, reset fore
+            print(Fore.RESET, end="")
+
+    if input() != "":
+        break
+print(Style.RESET_ALL)
