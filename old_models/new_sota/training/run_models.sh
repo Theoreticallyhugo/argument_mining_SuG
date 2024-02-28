5dc52edeaf3e1eb6ff4f820bc24568bfda68fd60
Theoreticallyhugo <me@theoreticallyhugo.com>
2024-02-28 23:30:47 +0100

    running tests offline since hugginface is down

--- old_models/new_sota/training/run_models.sh
+++ old_models/new_sota/training/run_models.sh
@@ -1,21 +1,21 @@
 # python full_labels.py && python spans.py && python simple.py && python sep_tok.py && python sep_tok_full_labels.py
-python train.py -m full_labels -e 4
-python train.py -m spans -e 4
-python train.py -m simple -e 4
-python train.py -m sep_tok -e 4
-python train.py -m sep_tok_full_labels -e 4
-python train.py -m full_labels
-python train.py -m spans
-python train.py -m simple
-python train.py -m sep_tok
-python train.py -m sep_tok_full_labels
-python train.py -m full_labels -e 6
-python train.py -m spans -e 6
-python train.py -m simple -e 6
-python train.py -m sep_tok -e 6
-python train.py -m sep_tok_full_labels -e 6
-python train.py -m full_labels -e 7
-python train.py -m spans -e 7
-python train.py -m simple -e 7
-python train.py -m sep_tok -e 7
-python train.py -m sep_tok_full_labels -e 7
+python train.py -m full_labels -e 4 -p
+python train.py -m spans -e 4 -p
+python train.py -m simple -e 4 -p
+python train.py -m sep_tok -e 4 -p
+python train.py -m sep_tok_full_labels -e 4 -p
+python train.py -m full_labels -p
+python train.py -m spans -p
+python train.py -m simple -p
+python train.py -m sep_tok -p
+python train.py -m sep_tok_full_labels -p
+python train.py -m full_labels -e 6 -p
+python train.py -m spans -e 6 -p
+python train.py -m simple -e 6 -p
+python train.py -m sep_tok -e 6 -p
+python train.py -m sep_tok_full_labels -e 6 -p
+python train.py -m full_labels -e 7 -p
+python train.py -m spans -e 7 -p
+python train.py -m simple -e 7 -p
+python train.py -m sep_tok -e 7 -p
+python train.py -m sep_tok_full_labels -e 7 -p
