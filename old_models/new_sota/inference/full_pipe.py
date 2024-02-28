d8822dfbab2b40d7fae227b9174ae85e88da88ac
Theoreticallyhugo <me@theoreticallyhugo.com>
2024-02-28 23:29:36 +0100

    coming closer to being able to fully run pipeline and save as brat standoff format, but huggingface is down

--- old_models/new_sota/inference/full_pipe.py
+++ old_models/new_sota/inference/full_pipe.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import re
 
 from collections import Counter
@@ -12,50 +13,66 @@ import spans_pipe
 
 
 def to_brat(text, pipe_out, verbose=False):
-    # search for <s> in the text, save the index, and look for the </s>.
-    # save its index too. then find all tokens from pipe_out that lie between
-    # these two separators, take the most frequent role and save it as a span
-    # FIXME: remove sep-toks from the text before returning brat format
+    """
+    expects a text with separation tokens, and the ouput of the sep_tok model.
+
+    returns the data in brat standoff format.
+    return (txt, ann)
+
+    search for <s> in the text, save the index, and look for the </s>.
+    save its index too. then find all tokens from pipe_out that lie between
+    these two separators, take the most frequent role and save it as a span
+    """
+    # find where each span begins and ends, by looking for sep_toks
     starts = [m.end(0) for m in re.finditer("<s>", text)]
-    # print(len(cls_toks))
     ends = [m.start(0) for m in re.finditer("</s>", text)]
-    # print(len(sep_toks))
 
     # remove sep_toks
     text = text.replace("<s>", "").replace("</s>", "")
     if verbose:
         input(text)
+    output_txt = text
 
     # adjust for removed sep_toks
     for i in range(len(starts)):
         starts[i] = starts[i] - 3 - (7 * i)
         ends[i] = ends[i] - 3 - (7 * i)
 
+    # find the string for each span
     span_texts = [text[start:end] for start, end in zip(starts, ends)]
     if verbose:
         input(span_texts)
+
+    # find the label for each span
     labels = []
-    for start, end in zip(starts, ends):
+    for start, end, index in zip(starts, ends, range(len(ends))):
         tmp_labels = []
         for result in pipe_out:
             if result["start"] >= start and result["end"] <= end:
                 tmp_labels.append(result["entity"])
-
-        # print(tmp_labels)
-        # input(Counter(tmp_labels).most_common(1)[0][0])
-
         # out of all labels found within the span, take the most frequent
         # and append to the list of labels
-        # FIXME: this crashes if there are no annotations in the span
-        #   remove span if this is the case?
-        labels.append(Counter(tmp_labels).most_common(1)[0][0])
+        if len(tmp_labels) == 0:
+            # no labels found within current span
 
-    # TODO: return this as proper file
+            # make sure to remove span and all associated data
+            starts.pop(index)
+            ends.pop(index)
+            span_texts.pop(index)
+        else:
+            labels.append(Counter(tmp_labels).most_common(1)[0][0])
+
+    # generate the output line by line in brat standoff format
+    output_ann = []
     for id, label, start, end, span_text in zip(
         range(len(labels)), labels, starts, ends, span_texts
     ):
-        print(f"T{id + 1}\t{label} {start} {end}\t{span_text}")
+        line = f"T{id + 1}\t{label} {start} {end}\t{span_text}"
+        print(line)
+        output_ann.append(line)
 
+    # if run in verbose mode, print the text with each span, coloured
+    # with its labels colour
     if verbose:
         indices = starts + ends
         indices.sort()
@@ -77,17 +94,24 @@ def to_brat(text, pipe_out, verbose=False):
                 print(Style.RESET_ALL, end="")
             print(text[indices[i - 1] : indices[i]], end="")
         print(Style.RESET_ALL + text[indices[-1] :])
-
         input()
+
+    return output_txt, output_ann
 
 
 if __name__ == "__main__":
+    # doesnt matter which config were loading as we need the untouched texts
     ds = datasets.load_dataset(
         "Theoreticallyhugo/essays_SuG", "sep_tok", trust_remote_code=True
     )
     texts = ds["test"]["text"]
+    ids = ds["test"]["id"]
 
     spans_results = spans_pipe.inference(texts)
     results = sep_tok_pipe.inference(spans_results)
-    for text, result in zip(texts, results):
-        to_brat(text, result)
+    for text, result, id in zip(texts, results, ids):
+        txt, ann = to_brat(text, result)
+        with open(Path(f"essay_{str(id).rjust(3, '0')}.txt"), "w") as w:
+            w.write(txt)
+        with open(Path(f"essay_{str(id).rjust(3, '0')}.ann"), "w") as w:
+            w.writelines(ann)
