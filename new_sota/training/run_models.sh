982483d854dd9859f60511b50813cc9f96c58f6f
Theoreticallyhugo <me@theoreticallyhugo.com>
2024-02-29 19:03:39 +0100

    adding commands for extended training run

--- new_sota/training/run_models.sh
+++ new_sota/training/run_models.sh
@@ -1,21 +1,42 @@
 # python full_labels.py && python spans.py && python simple.py && python sep_tok.py && python sep_tok_full_labels.py
-python train.py -m full_labels -e 4 -p
-python train.py -m spans -e 4 -p
-python train.py -m simple -e 4 -p
-python train.py -m sep_tok -e 4 -p
-python train.py -m sep_tok_full_labels -e 4 -p
-python train.py -m full_labels -p
-python train.py -m spans -p
-python train.py -m simple -p
-python train.py -m sep_tok -p
-python train.py -m sep_tok_full_labels -p
-python train.py -m full_labels -e 6 -p
-python train.py -m spans -e 6 -p
-python train.py -m simple -e 6 -p
-python train.py -m sep_tok -e 6 -p
-python train.py -m sep_tok_full_labels -e 6 -p
-python train.py -m full_labels -e 7 -p
-python train.py -m spans -e 7 -p
-python train.py -m simple -e 7 -p
-python train.py -m sep_tok -e 7 -p
-python train.py -m sep_tok_full_labels -e 7 -p
+# python train.py -m full_labels -e 4 -p
+# python train.py -m spans -e 4 -p
+# python train.py -m simple -e 4 -p
+# python train.py -m sep_tok -e 4 -p
+# python train.py -m sep_tok_full_labels -e 4 -p
+#
+# python train.py -m full_labels -p
+# python train.py -m spans -p
+# python train.py -m simple -p
+# python train.py -m sep_tok -p
+# python train.py -m sep_tok_full_labels -p
+#
+# python train.py -m full_labels -e 6 -p
+# python train.py -m spans -e 6 -p
+# python train.py -m simple -e 6 -p
+# python train.py -m sep_tok -e 6 -p
+# python train.py -m sep_tok_full_labels -e 6 -p
+#
+# python train.py -m full_labels -e 7 -p
+# python train.py -m spans -e 7 -p
+# python train.py -m simple -e 7 -p
+# python train.py -m sep_tok -e 7 -p
+# python train.py -m sep_tok_full_labels -e 7 -p
+
+python train.py -m full_labels -e 8 -p
+python train.py -m spans -e 8 -p
+python train.py -m simple -e 8 -p
+python train.py -m sep_tok -e 8 -p
+python train.py -m sep_tok_full_labels -e 8 -p
+
+python train.py -m full_labels -e 9 -p
+python train.py -m spans -e 9 -p
+python train.py -m simple -e 9 -p
+python train.py -m sep_tok -e 9 -p
+python train.py -m sep_tok_full_labels -e 9 -p
+
+python train.py -m full_labels -e 10 -p
+python train.py -m spans -e 10 -p
+python train.py -m simple -e 10 -p
+python train.py -m sep_tok -e 10 -p
+python train.py -m sep_tok_full_labels -e 10 -p
