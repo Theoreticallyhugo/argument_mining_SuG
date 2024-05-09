a0dbd63e0b7decbe847049b5bf5c62dde87a576e
Theoreticallyhugo <me@theoreticallyhugo.com>
2024-05-09 22:23:09 +0200

    added license (AGPLv3) to all files except readme and jupyter noteboots

--- old_models/simple_find_label_span/data.py
+++ old_models/simple_find_label_span/data.py
@@ -1,3 +1,22 @@
+"""
+    argument_mining_SuG is aimed at improving argument component 
+    identification and classification based on Stab and Gurevychs prior work.
+    Copyright (C) 2024  Hugo Meinhof (Theoreticallyhugo)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import os
 import json
 from datasets import Dataset
